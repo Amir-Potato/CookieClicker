df75b7f04fc66d55cb0757ab213232696a0bc561
BakedPotato101 <jackson.baker7562@gmail.com>
2023-04-19 21:10:04 -0500

    modified navbar and main styles

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,18 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnDestroy, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { WindowService } from 'src/app/engine/window.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
 
   windowService = inject(WindowService);
 
+  windowSubscription: Subscription;
+
   constructor() {
-    this.windowService.mouseData$.subscribe((mouse) => {
-      
-    })
+    this.windowSubscription = this.windowService.window$.subscribe((window) => {
+      window.document.title = 'Home';
+  });
+}
+  ngOnDestroy(): void {
+    this.windowSubscription.unsubscribe();
   }
+
+
+
 }
