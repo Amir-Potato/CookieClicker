020bb452a71f1e1d78a9c54221bad059cb1c5734
BakedPotato101 <jackson.baker7562@gmail.com>
2023-04-21 00:06:10 -0500

    added not-found view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
 import { GameComponent } from './views/game/game.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'game/:id', component: GameComponent },
-  { path: '**', redirectTo: 'home' },
+  { path: '404', component: NotFoundComponent},
+  { path: '', redirectTo: 'home' , pathMatch: 'full'},
+  { path: '**', redirectTo: '404' },
+
 ];
 
 @NgModule({
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    GameComponent
+    GameComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule
