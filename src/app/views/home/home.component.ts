a7fb794d4b6a799e33a0e00360ae2bbb6bd09ccb
BakedPotato101 <jackson.baker7562@gmail.com>
2023-04-25 00:44:50 -0500

    added home menu

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs';
 import { WindowService } from 'src/app/engine/window.service';
 
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,14 +12,39 @@ import { WindowService } from 'src/app/engine/window.service';
 export class HomeComponent implements OnDestroy {
 
   windowService = inject(WindowService);
-
+  isTrue:boolean = false;
+  
   windowSubscription: Subscription;
 
   constructor() {
     this.windowSubscription = this.windowService.window$.subscribe((window) => {
       window.document.title = 'Home';
   });
-}
+  }
+
+  newGame() {
+    console.log('new game');
+    this.isTrue = !this.isTrue;
+  }
+
+
+  loadGame() {
+    console.log('load game');
+
+  }
+
+
+  loadOptions() {
+    console.log('load options');
+
+  }
+
+
+  loadCredits() {
+    console.log('load credits');
+
+  }
+  
   ngOnDestroy(): void {
     this.windowSubscription.unsubscribe();
   }
