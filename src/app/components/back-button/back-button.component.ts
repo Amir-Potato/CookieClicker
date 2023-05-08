fd437c298d5a47b84ce008c971bfc4f236496135
BakedPotato101 <jackson.baker7562@gmail.com>
2023-05-07 21:57:40 -0500

    added back button

--- src/app/components/back-button/back-button.component.ts
+++ src/app/components/back-button/back-button.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
+import { StoreService } from '../../engine/services/store.service';
+import type { menuItems } from '../../engine/types/menuItems';
 
 @Component({
   selector: 'app-back-button',
   templateUrl: './back-button.component.html',
-  styleUrls: ['./back-button.component.scss']
+  styleUrls: ['./back-button.component.scss'],
 })
 export class BackButtonComponent {
+  @Input('sub-menu') subMenu: menuItems = 'title';
+  store = inject(StoreService);
 
+  constructor() {}
+
+  back() {
+    this.store.currentSubmenu.next(this.subMenu);
+  }
 }
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CreditsComponent } from './credits/credits.component';
 import { BackButtonComponent } from './back-button/back-button.component';
 
+
 @NgModule({
   declarations: [CreditsComponent, BackButtonComponent],
   imports: [CommonModule],
